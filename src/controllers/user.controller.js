6dd7d12e8c100861062b99dbe35ec0266c821f07
MoxitParmar <moxitparmar2004@gmail.com>
2024-05-06 11:06:31 +0530

    chore: Add getUserChannelProfile and getWatchHistory endpoints with aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"; // Import the User model
 import { uploadOnCloudinary } from "../utils/cloudinary.js"; // Import the uploadOnCloudinary utility function to upload images to Cloudinary
 import { apiResponce } from "../utils/apiResponce.js"; // Import the apiResponce class
 import jwt from "jsonwebtoken"; // Import the jsonwebtoken package
+import mongoose from "mongoose";
 
 // step 5: Generate access and refresh token method
 const generateAccessTokenAndRefreshToken = async (userID) => {
@@ -302,7 +303,116 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   return res.status(200).json(new apiResponce(200, user, "Cover image updated successfully"));
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
 
+  if (!username?.trim()) {
+    throw new apiError(400, "Username is required");
+  }
+
+  // aggregate query to get the user channel profile
+  const channel = await User.aggregate([
+    {
+      // match the username from the request
+      $match: { username: username?.toLowerCase() }
+    },
+    {
+      // Lookup the subscriptions collection to get the subscribers
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+      
+    },
+    // lookup the subscriptions collection to get the channels subscribed to
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    // add fields to the result
+    {
+      $addFields: {
+        subscriberCount: { $size: "$subscribers" },
+        channelsSubscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"]
+            },
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    // project the fields to return in the response
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscriberCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+      }
+    }
+  ])
+
+  if (!channel?.length) {
+    throw new apiError(404, "Channel not found");
+  }
+
+  return res.status(200).json(new apiResponce(200, channel[0], "Channel found successfully"));
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(req.user._id) }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          }, {
+            $addFields: {
+              owner: { $first: "$owner"}
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res.status(200).json(new apiResponce(200, user[0]?.watchHistory || [], "Watch history found successfully"));
+})
 
 // Export the registerUser function
 export {
@@ -314,5 +424,7 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express"; // Import the Router method from the express package
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"; // Import the registerUser controller function
+import {
+  loginUser, 
+  logoutUser, 
+  registerUser, 
+  refreshAccessToken, 
+  changeCurrentPassword, 
+  getWatchHistory,
+  getCurrentUser, 
+  updateAccountDetails, 
+  updateUserAvatar, 
+  updateUserCoverImage, 
+  getUserChannelProfile
+} from "../controllers/user.controller.js"; // Import the registerUser controller function
 import { upload } from "../middlewares/multer.middleware.js"; // Import the upload middleware
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,5 +39,14 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
 
 export default router;
